55280cd0b1e75afe6f437d83cbec6022bc81ca7e
ludovicogatta <123467911+ludovicogatta@users.noreply.github.com>
2023-01-26 08:40:40 +0100

    Update main-4.cpp

--- main-4.cpp
+++ main-4.cpp
@@ -1,8 +1,16 @@
-
 #include<fstream>
 #include <iostream>
-
 using namespace std;
+//Categoria, Marca, Modello, Colore, L/A, 
+struct Auto
+{
+    string categoria;
+    string marca;
+    string modello;
+    string colore;
+    string lunedi, martedi,mercoledi,giovedi,venerdi,sabato,domenica;
+    
+};
 void  stampaf()
 {
     string s;
@@ -15,8 +23,53 @@ void  stampaf()
     fin.close();
 }
 
+void caricamentoVet(Auto vet[])
+{
+      ifstream fin("auto.txt",ios::in);
+    while(!fin.eof())
+    {
+        for(int i=0;i<7;i++)
+        {
+            fin>>vet[i].categoria>>vet[i].marca>>vet[i].modello>>vet[i].colore>>vet[i].lunedi>>vet[i].martedi>>vet[i].mercoledi>>vet[i].giovedi>>vet[i].venerdi>>vet[i].sabato>>vet[i].domenica;
+            
+        }
+    }
+    fin.close();
+    
+}
+
+void aggiornaFile(autod auto_vet[])
+{
+    ofstream fout("auto.txt");
+    
+    for(int v=0; v<7; v++)
+    {
+        fout<<endl<<auto_vet[v].categoria<<" "<<auto_vet[v].marca<<" "<<auto_vet[v].modello<<" "<<auto_vet[v].colore<<" ";
+        fout<<auto_vet[v].lu<<" "<<auto_vet[v].ma<<" "<<auto_vet[v].me<<" "<<auto_vet[v].gi<<" "<<auto_vet[v].ve<<" "<<auto_vet[v].sa<<" "<<auto_vet[v].dom;
+    }
+    
+    fout.close();
+}
+void prenota_auto(Auto vet[])
+{
+    string cat;
+    int gio;
+    char oc;
+    cout<<"scegli bene la categoria dell' auto che vuoi noleggiare: "<<endl;
+    cout<<"inserisci la categoria dell' auto scelta:";
+    cin>>cat;
+    cout<<"inserisci i giorni: ";
+    cin>>gio;
+  
+    
+
+}
+    
+
+
 int main()
 {
+    Auto vet[14];
     int scelta;
     
     do
